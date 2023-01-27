3947af23e536cc479001e8164c0aa0371091ef61
pawkal <kalel7410@gmail.com>
2023-01-27 19:54:47 +0100

    Add WalkPageViewModel

---
+++ TrackMyWalks/ViewModels/WalkBaseViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace TrackMyWalks.ViewModels
+{
+    public abstract class WalkBaseViewModel :
+        INotifyPropertyChanged
+    {
+        protected WalkBaseViewModel() 
+        {
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged
+            ([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ TrackMyWalks/ViewModels/WalkPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using TrackMyWalks.Models;
+
+namespace TrackMyWalks.ViewModels
+{
+    public class WalkPageViewModel : WalkBaseViewModel
+    {
+        ObservableCollection<WalkEntries> _walkEntries;
+
+        public ObservableCollection<WalkEntries> walkEntries
+        {
+            get { return _walkEntries; }
+            set { _walkEntries = value;
+                OnPropertyChanged();
+
+            }
+        }
+    }
+}
