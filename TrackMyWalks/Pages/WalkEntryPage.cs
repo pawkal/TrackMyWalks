68735a5b105670fb4f91ea05c60a5cff7099e324
pawkal <kalel7410@gmail.com>
2023-01-28 15:48:32 +0100

    another changes page 70

--- TrackMyWalks/Pages/WalkEntryPage.cs
+++ TrackMyWalks/Pages/WalkEntryPage.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 
 using Xamarin.Forms;
+using TrackMyWalks.Models;
+
 
 namespace TrackMyWalks
 {
@@ -13,23 +15,29 @@ namespace TrackMyWalks
         {
             Title = "Nowy wpis";
 
+            BindingContext = new WalkEntryViewModel();
+
             var walkTitle = new EntryCell
             {
                 Label = "Tytuł:",
                 Placeholder = "Nazwa szlaku"
             };
+            walkTitle.SetBinding(EntryCell.TextProperty, "Tytuł", BindingMode.TwoWay);
 
             var walkNotes = new EntryCell
             {
                 Label = "Uwagi:",
                 Placeholder = "Opis"
             };
+            walkNotes.SetBinding(EntryCell.TextProperty, "Uwagi", BindingMode.TwoWay);
+
             var walkLatitude = new EntryCell
             {
                 Label = "Szerokość geograficzna:",
                 Placeholder = "Szerokość",
                 Keyboard = Keyboard.Numeric
             };
+            walkLatitude.SetBinding(EntryCell.TextProperty, "Szerokość geograficzna", BindingMode.TwoWay);
 
             var walkLongitude = new EntryCell
             {
@@ -37,6 +45,7 @@ namespace TrackMyWalks
                 Placeholder = "Długosć",
                 Keyboard = Keyboard.Numeric
             };
+            walkLongitude.SetBinding(EntryCell.TextProperty, "Długość geograficzna", BindingMode.TwoWay);
 
             var walkKilometers = new EntryCell
             {
@@ -44,18 +53,21 @@ namespace TrackMyWalks
                 Placeholder = "Liczba kilometrów",
                 Keyboard = Keyboard.Numeric
             };
+            walkKilometers.SetBinding(EntryCell.TextProperty, "Liczba kilometrów", BindingMode.TwoWay);
 
             var walkDifficulty = new EntryCell
             {
                 Label = "Poziom trudności szlaku:",
                 Placeholder = "Poziom trudnosci szlaku"
             };
+            walkDifficulty.SetBinding(EntryCell.TextProperty, "Poziom trudności", BindingMode.TwoWay);
 
             var walkImageUrl = new EntryCell
             {
                 Label = "URL obrazu",
                 Placeholder = "Url obrazu"
             };
+            walkImageUrl.SetBinding(EntryCell.TextProperty, "URL obrazu", BindingMode.TwoWay);
 
             Content = new TableView
             {
@@ -79,6 +91,9 @@ namespace TrackMyWalks
             {
                 Text = "Zapisz"
             };
+
+            saveWalkItem.SetBinding(MenuItem.CommandProperty, "SaveCommand");
+
             saveWalkItem.Clicked += (sender, e) =>
             {
                 Navigation.PopToRootAsync(true);
---
+++ TrackMyWalks/ViewModels/WalkEntryViewModel.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using TrackMyWalks.Models;
+using Xamarin.Forms;
+
+namespace TrackMyWalks.ViewModels
+{
+    public class WalkEntryViewModel : WalkBaseViewModel
+    {
+        string _title;
+            public string title
+        {
+            get { return _title; }
+            set
+            {
+                _title = value;
+                OnPropertyChanged();
+                SaveCommand.ChangeCanExecute();
+            }
+        }
+        string _notes;
+        public string notes
+        {
+            get { return _notes; }
+            set
+            {
+                _notes = value;
+                OnPropertyChanged();
+            }
+        }
+        double _latitude;
+        public double latitude
+        {
+            get { return _latitude; }
+            set
+            {
+                _latitude = value;
+                OnPropertyChanged();
+            }
+        }
+        double _longitude;
+        public double longitude
+        {
+            get { return _longitude; }
+            set
+            {
+                _longitude = value;
+                OnPropertyChanged();
+            }
+        }
+        double _kilometers;
+        public double kilometers
+        {
+            get { return _kilometers;}
+            set
+            {
+                _kilometers = value;
+                OnPropertyChanged();
+            }
+        }
+        string _difficulty;
+        public string difficulty
+        {
+            get { return _difficulty; }
+            set
+            {
+                _difficulty = value;
+                OnPropertyChanged();
+            }
+        }
+        double _distance;
+        public double distance
+        {
+            get { return _distance; }
+            set
+            {
+                _distance = value;
+                OnPropertyChanged();
+            }
+        }
+        string _imageURL;
+        public string ImageURL
+        {
+            get { return _imageURL; }
+            set
+            {
+                _imageURL = value;
+                OnPropertyChanged();
+            }
+        }
+        public WalkEntryViewModel()
+        {
+            title = "Nowy Szlak";
+            difficulty = "Niski";
+            distance = 1.0;
+        }
+        Command _saveCommand;
+        public Command SaveCommand
+        {
+            get
+            {
+                return _saveCommand ?? (_saveCommand = new Command(ExecuteSaveCommand, ValidateFormDetails));
+            }
+        }
+        void ExecuteSaveCommand()
+        {
+            var newWalkItem = new WalkEntries
+            {
+                Title = this.Title,
+                Notes = this.Notes,
+                Latitude = this.Latitude,
+                Longitude = this.Longitude,
+                Kilometers = this.Kilometers,
+                Difficulty = this.Difficulty,
+                Distance = this.Distance,
+                ImageUrl = this.ImageURL,
+            };
+
+        }
+        bool ValidateFormDetails()
+        {
+            return !string.IsNullOrWhiteSpace(title);
+        }
+    }
+}
