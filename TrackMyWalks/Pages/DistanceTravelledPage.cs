201309fe7b99d1d0b2780dc9f76f3021af321c04
pawkal <kalel7410@gmail.com>
2023-01-29 14:26:47 +0100

    capter 2 ended

--- TrackMyWalks/Pages/DistanceTravelledPage.cs
+++ TrackMyWalks/Pages/DistanceTravelledPage.cs
@@ -1,85 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 using TrackMyWalks.Models;
-
-
+using TrackMyWalks.ViewModels;
 
 namespace TrackMyWalks
 {
     public class DistanceTravelledPage : ContentPage
     {
+        DistTrevelledViewModels _viewModel
+        {
+            get { return BindingContext as DistTrevelledViewModels; }
+        }
+
         public DistanceTravelledPage(WalkEntries walkItem)
         {
-            Title = "Przebyty dystans";
+            Title = "Distance Travelled";
 
+          
+            BindingContext = new DistTrevelledViewModels(walkItem);
+
+            
             var trailMap = new Map();
 
+            
             trailMap.Pins.Add(new Pin
             {
                 Type = PinType.Place,
-                Label = walkItem.Title,
-                Position = new Position(walkItem.Latitude, walkItem.Longitude)
+                Label = _viewModel.WalkEntry.Title,
+                Position = new Position(_viewModel.WalkEntry.Latitude, _viewModel.WalkEntry.Longitude)
             });
-            trailMap.MoveToRegion(MapSpan.FromCenterAndRadius(new
-                Position(walkItem.Latitude, walkItem.Longitude),
-                Distance.FromKilometers(1.0)));
+
+            
+            trailMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(_viewModel.WalkEntry.Latitude, _viewModel.WalkEntry.Longitude), Distance.FromKilometers(1.0)));
 
             var trailNameLabel = new Label()
             {
                 FontSize = 18,
                 FontAttributes = FontAttributes.Bold,
                 TextColor = Color.Black,
-                Text = walkItem.Title
+                HorizontalTextAlignment = TextAlignment.Center
             };
+
+            trailNameLabel.SetBinding(Label.TextProperty, "WalkEntry.Title");
 
             var trailDistanceTravelledLabel = new Label()
             {
                 FontAttributes = FontAttributes.Bold,
                 FontSize = 20,
                 TextColor = Color.Black,
-                Text = "Przebyty dystans:",
                 HorizontalTextAlignment = TextAlignment.Center
             };
 
-            var totalDistanceTaken = new Label()
-            {
-                FontAttributes = FontAttributes.Bold,
-                FontSize = 20,
-                TextColor = Color.Black,
-                Text = $"{walkItem.Distance} km",
-                HorizontalTextAlignment = TextAlignment.Center
-            };
+            trailDistanceTravelledLabel.SetBinding(Label.TextProperty, "Travelled", stringFormat: "Distance Travelled: {0} km");
 
             var totalTimeTakenLabel = new Label()
             {
                 FontAttributes = FontAttributes.Bold,
                 FontSize = 20,
                 TextColor = Color.Black,
-                Text = "Czas:",
                 HorizontalTextAlignment = TextAlignment.Center
             };
 
-            var totalTimeTaken = new Label()
-            {
-                FontAttributes = FontAttributes.Bold,
-                FontSize = 20,
-                TextColor = Color.Black,
-                Text = "0 h 0 m 0 s",
-                HorizontalTextAlignment = TextAlignment.Center
-            };
+            totalTimeTakenLabel.SetBinding(Label.TextProperty, "TimeTaken", stringFormat: "Time Taken: {0}");
 
             var walksHomeButton = new Button
             {
                 BackgroundColor = Color.FromHex("#008080"),
                 TextColor = Color.White,
-                Text = "Zakończ ten szlak"
+                Text = "End this Trail"
             };
 
+          
             walksHomeButton.Clicked += (sender, e) =>
             {
                 if (walkItem == null) return;
@@ -94,15 +85,12 @@ namespace TrackMyWalks
                 {
                     Orientation = StackOrientation.Vertical,
                     HorizontalOptions = LayoutOptions.FillAndExpand,
-                    Children =
-                    {
-                        trailMap,
-                        trailNameLabel,
-                        trailDistanceTravelledLabel,
-                        totalDistanceTaken,
-                        totalTimeTakenLabel,
-                        totalTimeTaken,
-                        walksHomeButton
+                    Children = {
+                    trailMap,
+                    trailNameLabel,
+                    trailDistanceTravelledLabel,
+                    totalTimeTakenLabel,
+                    walksHomeButton
                     }
                 }
             };
---
+++ TrackMyWalks/ViewModels/DistTravelledViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TrackMyWalks.Models;
+using TrackMyWalks.ViewModels;
+
+namespace TrackMyWalks.ViewModels
+{
+   public class DistTrevelledViewModels : WalkBaseViewModel
+    {
+        WalkEntries _walkEntry;
+
+        public WalkEntries WalkEntry
+        {
+            get { return _walkEntry; }
+            set
+            {
+                _walkEntry = value;
+                OnPropertyChanged();
+            }
+        }
+        double _travelled;
+            public double Travelled
+        {
+            get { return _travelled; }
+            set
+            {
+                _travelled = value;
+                OnPropertyChanged();
+            }
+        }
+        double _hours;
+        public double Hours
+        {
+            get { return _hours; }  
+            set
+            {
+                _hours = value;
+                OnPropertyChanged();
+            }
+        }
+        double _minutes;
+        public double minutes
+        {
+            get { return _minutes; }
+            set
+            {
+                _minutes = value;
+                OnPropertyChanged();
+            }
+        }
+        double _seconds;
+        public double seconds
+        {
+            get { return _seconds; }
+            set
+            {
+                _seconds = value;
+                OnPropertyChanged();
+
+            }
+        }
+        public string TimeTaken
+        {
+            get
+            {
+                return string.Format("{0:00{ 1:00}:{ 2:00}", this.Hours, this.minutes, this.seconds);
+            }
+        }
+
+        public DistTrevelledViewModels(WalkEntries walkEntry) 
+        {
+            this.Hours = 0;
+            this.minutes= 0;
+            this.seconds = 0;
+            this.Travelled = 0;
+
+            WalkEntry = walkEntry;
+
+        }
+    }
+}
